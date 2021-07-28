2c74dd0ccbe3a82070a174b9ca1daa99fdf8f0f3
Rafael <rafaelsouzavalle@gmail.com>
2021-07-28 17:36:26 -0300

    Update HelloController.kt
    Insira as anotações gerar um endpoint de teste

--- src/main/kotlin/br/com/digivalle/controller/HelloController.kt
+++ src/main/kotlin/br/com/digivalle/controller/HelloController.kt
@@ -1,4 +1,13 @@
 package br.com.digivalle.controller
 
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+
+@Controller("/hello")
 class HelloController {
+
+    @Get
+    fun hello(): String {
+        return "Hello Micronaut!"
+    }
 }
